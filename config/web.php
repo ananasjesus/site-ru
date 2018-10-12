26e49d70f371d8ae1fc6385cdb804ef8466678e5
ananasjesus <i@i.ru>
2018-10-12 19:47:35 +0300

    add cookie key

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '82688554',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
