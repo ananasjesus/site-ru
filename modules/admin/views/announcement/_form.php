8ec3a4a0bf3926a32888ef626ad06bfa2b351819
ananasjesus <i@i.ru>
2018-10-30 21:39:28 +0400

    создан круд для категорий и добавлен для объявлений

---
+++ modules/admin/views/announcement/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Announcement */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="announcement-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'viewed')->textInput() ?>
+
+    <?= $form->field($model, 'user_id')->textInput() ?>
+
+    <?= $form->field($model, 'expired')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
