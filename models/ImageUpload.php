abd2cd61972f058b36a171ec6c7ad405a51be853
ananasjesus <i@i.ru>
2018-11-01 22:07:23 +0400

    improve image upload

--- models/ImageUpload.php
+++ models/ImageUpload.php
@@ -8,12 +8,22 @@ use yii\web\UploadedFile;
 
 class ImageUpload extends Model {
 
+    const IMAGE_PATH = 'uploads/';
+
     public $image;
 
     public function uploadFile(UploadedFile $file)
     {
-        $file->saveAs(Yii::getAlias('@web') . 'uploads/' . $file->name);
-        return $file->name;
+        $this->image = $file;
+
+        $fullPath = Yii::getAlias('@web') . self::IMAGE_PATH;
+
+        do {
+            $filename = $fullPath . uniqid($file->name, true) . '.' . $file->extension;
+        } while(file_exists($filename));
+
+        $file->saveAs($filename);
+        return $filename;
     }
 
 }
