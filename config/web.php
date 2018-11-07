19462c4678bfde1126752379bdcc913552e8a46d
ananasjesus <i@i.ru>
2018-11-07 21:10:10 +0400

    add app name

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'Доска объявлений',
     'language' => 'ru-RU',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
