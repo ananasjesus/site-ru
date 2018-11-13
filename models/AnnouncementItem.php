599cf9a9ba7d5e820c37a6c63968240a9b6a805f
ananasjesus <i@i.ru>
2018-11-13 17:57:34 +0400

    fix sql

--- models/AnnouncementItem.php
+++ models/AnnouncementItem.php
@@ -5,23 +5,24 @@ namespace app\models;
 use app\models\CategoryAnnouncement;
 use yii\data\ActiveDataProvider;
 
-class AnnouncementItem extends CategoryAnnouncement
+class AnnouncementItem extends Announcement
 {
-    public $id, $title, $content, $viewed, $created, $expired, $user_id, $username, $image;
 
     public static function findByCategory($id)
     {
+        $now = date('Y-m-d', time());
         $query = self::find()
-            ->join('LEFT JOIN', 'announcement', 'category_announcement.announcement_id = announcement.id')
+            ->join('LEFT JOIN', 'category_announcement', 'announcement.id = category_announcement.announcement_id')
             ->join('LEFT JOIN', 'user', 'announcement.user_id = user.id')
             ->select(['announcement.id AS id', 'announcement.title AS title', 'announcement.content AS content',
                 'announcement.viewed AS viewed', 'announcement.created AS created', 'announcement.expired AS expired',
-                'announcement.user_id AS user_id', 'announcement.image AS image', 'user.name AS username'])
+                'announcement.user_id AS user_id', 'announcement.image AS image', 'user.name AS name'])
             ->orderBy('created DESC')
-        ->distinct();
+            ->where("announcement.expired > $now")
+            ->distinct();
 
         if ($id !== null)
-            $query = $query->where(['category_announcement.category_id' => $id]);
+            $query = $query->andWhere(['category_announcement.category_id' => $id]);
 
         $provider = new ActiveDataProvider([
             'query' => $query,
@@ -33,11 +34,4 @@ class AnnouncementItem extends CategoryAnnouncement
         return $provider;
     }
 
-    public function getImage()
-    {
-        if(is_file($this->image))
-            return $this->image;
-        else
-            return 'no_image.png';
-    }
 }
