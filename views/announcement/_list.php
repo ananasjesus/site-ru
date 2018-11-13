f068a60a3e862e1faac6caba4cf583e28a27a87c
ananasjesus <i@i.ru>
2018-11-13 18:46:39 +0400

    fix design

--- views/announcement/_list.php
+++ views/announcement/_list.php
@@ -9,11 +9,11 @@ use yii\helpers\HtmlPurifier;
             <div class="col-md-3">
                 <img class="card-img-left" style="width: 20rem; height: 20rem; object-fit: cover; border-radius: inherit;" src="<?= Yii::getAlias('@web/') . $model->getImage();?>" alt="">
             </div>
-
+            <div class="col-md-9">
             <h4><?= strlen($model->title) < 100 ? $model->title : mb_substr($model->title, 0, 100) . '...' ?></h4>
             <h5><?= strlen($model->content) < 300 ? $model->content : mb_substr($model->content, 0, 300) . '...'?></h5>
             <a href="<?= Yii::getAlias('@web/announcement/view?id=') . $model->id ?>" class="btn btn-primary card-text-bottom">Подробнее</a>
-
+            </div>
         </div>
     </div>
 </div>
