92818aa82d9256c0fce6de2db797dcf06d172ecc
ananasjesus <i@i.ru>
2018-11-13 18:15:26 +0400

    fix design

--- views/announcement/_list.php
+++ views/announcement/_list.php
@@ -4,14 +4,16 @@ use yii\helpers\HtmlPurifier;
 ?>
 
 <div class="news-item">
-    <div class="card" style="height: 26rem; width: 90%; border:2px solid #b4b2b3; border-radius: 8px; background-color: #5b92cb">
-        <div class="card-header">
-            <?= strlen($model->title) < 100 ? $model->title : mb_substr($model->title, 0, 100) . '...' ?>
-        </div>
-        <div class="card-body" style="margin: 1rem;">
-            <img class="card-img-left" style="width: 20rem; height: 20rem; object-fit: cover; border-radius: inherit;" src="<?= Yii::getAlias('@web/') . $model->getImage();?>" alt="">
-            <p class="card-text"><?= strlen($model->content) < 300 ? $model->content : mb_substr($model->content, 0, 300) . '...'?></p>
+    <div class="card" style="width: 90%; border:2px solid #b4b2b3; border-radius: 8px; background-color: #5b92cb">
+        <div class="card-body row" style="margin: 1rem;">
+            <div class="col-md-3">
+                <img class="card-img-left" style="width: 20rem; height: 20rem; object-fit: cover; border-radius: inherit;" src="<?= Yii::getAlias('@web/') . $model->getImage();?>" alt="">
+            </div>
+
+            <h4><?= strlen($model->title) < 100 ? $model->title : mb_substr($model->title, 0, 100) . '...' ?></h4>
+            <h5><?= strlen($model->content) < 300 ? $model->content : mb_substr($model->content, 0, 300) . '...'?></h5>
             <a href="<?= Yii::getAlias('@web/announcement/view?id=') . $model->id ?>" class="btn btn-primary card-text-bottom">Подробнее</a>
+
         </div>
     </div>
 </div>
